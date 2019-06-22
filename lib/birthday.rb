d09d73d28f884230bf7f995411401aaf0b610c53
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-22 15:55:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,6 +17,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age <= 12
       message = "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts 'You are too old for this'
     end
   end
   message
